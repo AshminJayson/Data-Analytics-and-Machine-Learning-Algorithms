84d0e1034e331ee510c155f0a142707cb17437af
Ashmin Jayson <ashminjayson10@gmail.com>
2022-09-26 21:28:09 +0530

    Proximity_of_Nominal_and_Numeric_Attributes

---
+++ Proximity_of_Nominal_and_Numeric_Attributes.py
@@ -0,0 +1,57 @@
+
+f = open('./Dataset1.txt', 'r')
+
+data = {}
+features = f.readline()
+for i in features.strip().split():
+    data[i] = []
+
+keys = list(data.keys())
+
+values = f.readlines()
+number_of_val = 0
+for i in values:
+    k = 0
+    number_of_val = number_of_val + 1
+    for j in i.split():
+        data[keys[k]].append(j)
+        k = k + 1
+
+number_of_val = number_of_val - 1
+
+print("Enter the nominal attributes : ")
+nom_attributes = list(map(str, input().strip().split()))
+print("Enter the numeric attributes : ")
+num_attributes = list(map(str, input().strip().split()))
+
+#Find dissimilarity matrix of nominal attributes
+
+disnom = []
+disnum = []
+
+for i in range(number_of_val):
+    temp = []
+    for j in range(number_of_val):
+        temp.append(0)
+    disnom.append(temp)
+    disnum.append(temp)
+
+for i in range(number_of_val):
+    for j in range(i):
+        m = 0
+        e_dist = 0
+        for k in nom_attributes:
+            if data[k][i] == data[k][j]:
+                m = m + 1
+        for k in num_attributes:
+            e_dist = e_dist + ((float(data[k][i]) - float(data[k][j])) ** 2)
+        disnom[i][j] = (len(nom_attributes) - m) / len(nom_attributes)
+        disnum[i][j] = e_dist ** 0.5
+
+print(disnum)
+print(disnom)
+
+
+    
+        
+
