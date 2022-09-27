85c58f937921c284d16bd9812f42ca0b920f9b56
Ashmin Jayson <ashminjayson10@gmail.com>
2022-09-27 20:24:52 +0530

    Addition of printer function

--- Proximity_of_Nominal_and_Numeric_Attributes.py
+++ Proximity_of_Nominal_and_Numeric_Attributes.py
@@ -1,4 +1,10 @@
 
+def printer(mat, number_of_vals):
+    for i in range(number_of_vals):
+        for j in range(i):
+            print("{0:.2f}".format(mat[i][j]), end = ' ')
+        print(" ")
+
 f = open('./Dataset1.txt', 'r')
 
 data = {}
@@ -9,40 +15,38 @@ for i in features.strip().split():
 keys = list(data.keys())
 
 values = f.readlines()
-number_of_val = 0
+number_of_vals = len(values)
 for i in values:
     k = 0
-    number_of_val = number_of_val + 1
     for j in i.split():
         data[keys[k]].append(j)
         k = k + 1
 
 
 print("Enter the nominal attributes : ")
-nom_attributes = list(map(str, input().strip().split()))
+nom_attr = list(map(str, input().strip().split()))
 print("Enter the numeric attributes : ")
-num_attributes = list(map(str, input().strip().split()))
+num_attr = list(map(str, input().strip().split()))
 
 #Find dissimilarity matrix of nominal and numeric attributes
 
-disnom = [[0 for x in range(number_of_val)] for x in range(number_of_val)]
-disnum = [[0 for x in range(number_of_val)] for x in range(number_of_val)]
+disnom = disnum = [[0 for x in range(number_of_vals)] for x in range(number_of_vals)]
 
-for i in range(number_of_val):
+
+for i in range(number_of_vals):
     for j in range(i):
         m = 0
         e_dist = 0
-        for k in nom_attributes:
+        for k in nom_attr:
             if data[k][i] == data[k][j]:
                 m = m + 1
-        for k in num_attributes:
+        for k in num_attr:
             e_dist = e_dist + ((float(data[k][i]) - float(data[k][j])) ** 2)
-        disnom[i][j] = (len(nom_attributes) - m) / len(nom_attributes)
+        disnom[i][j] = (len(nom_attr) - m) / len(nom_attr)
         disnum[i][j] = e_dist ** 0.5
 
-print(disnum)
-print(disnom)
-
+printer(disnom, number_of_vals)
+printer(disnum, number_of_vals)
 
     
         
