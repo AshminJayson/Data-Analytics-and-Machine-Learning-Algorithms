63689e77d68ec0a9a9ebe135afe5d10786423f37
Ashmin Jayson <ashminjayson10@gmail.com>
2022-10-17 20:25:55 +0530

    Added Apriori Algorithm

--- Experiment_4_Apriori_Algorithm/apriorialgorithm.py
+++ Experiment_4_Apriori_Algorithm/apriorialgorithm.py
@@ -1,33 +1,70 @@
-data = {}
+data = []
 
 attr1 = "Transaction ID"
 attr2 = "List of items"
 
 n = int(input("Enter the number of data points : "))
 
-items = set()
+items = []
 for i in range(n):
 	at1 = input("Enter the transaction ID : ")
 	at2 = list(map(str, input("Enter the list of items associated with the input transaction ID : ").split(" ")))
-	data[at1] = sorted(at2)
-	for i in at2 : items.add([i])
+	data.append([at1] + at2)
+	for j in at2 :
+		if [j] not in items :  
+			items.append([j])
 	
-
 
 min_sup = int(input("Enter the minimum support threshold value : "))
 
 
 for i, j in enumerate(items):
-	print(items)
+	# print(items)
 	for k, l in enumerate(items):
-		if l > j and len(i) == len(k) :
+		if k > i and len(j) == len(l) :
 			flag = 1 
-			for m in range(len(i) - 1):
-				if i[m] != k[m]:
+			for m in range(len(j) - 1):
+				if j[m] != l[m]:
 					flag = 0
 					break
-			if flag == 0 : 
-				items.add([set(i + k)])
+			if flag == 1 :
+				temp = []
+				temp.extend(j)
+				temp.append(l[len(l) - 1])
+				if temp not in items:
+					items.append(temp)
+
+satissets = []
+max_l = 0
+for i in items:
+	count = 0
+	for dt in data:
+		flag1 = 1
+		for k in i:
+			flag2 = 1
+			for l in dt:
+				if k == l:
+					flag2 = 0
+			if flag2 == 1:
+				flag1 = 0
+				break
+		if flag1 == 1:
+			count += 1
+	
+	if count >= min_sup:
+		max_l = max(max_l, len(i))
+		satissets.append(i)
+
+for i in range(0, max_l):
+	print("The {i} item set is : {j}".format(i = i + 1, j = [l for l in satissets if len(l) == i + 1]))
+		
+
+
+		
+		
+
+
+
 	 
 			
 	
